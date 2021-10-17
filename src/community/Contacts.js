69551b3c6ee1cd6281f2e0670f6c0681bb522908
Aegis <killcrafter.lol@gmail.com>
2021-10-18 03:58:06 +1100

    Ajout des Informations de Contact

---
+++ src/community/Contacts.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Contacts = () => {
+    return (
+        <div id = "contact">
+            <ul><a href="mailto:wordletthewordwallet@gmail.com" ><img src = './mail.png' class="mail" alt=""/></a></ul>
+            <ul><a href="https://discord.gg/zmRRJcVJzM"><img src = "<>discord.png" class="discord"  alt=""/></a></ul>
+            <ul><a href="https://www.linkedin.com/company/wordlet/"><img src = "./linkedIn.png" class="linkedIn" alt=""/></a></ul>
+            <ul><a href="https://www.facebook.com/WordletTheWordWallet"><img src = "./fb.png" class="fb" alt=""/></a></ul>
+        </div>
+    )
+}
+
+export default Contacts
