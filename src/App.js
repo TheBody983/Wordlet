3ddd18df536573da9a1a67419a96b7701cdc92f0
Gregordy <bakatokodomowashojiki@hotmail.fr>
2021-10-18 17:39:59 +1100

    Fix App indentation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import UserData from './UserData';
 import Balance from './MyBalance';
 
 function App() {
-return (
-	<div className="App">
-	<h2> Mes Tokens </h2>
-	<UserData />
-	<Balance />
-	
-	</div>
-);
+	return (
+		<div className="App">
+			<h2> Mes Tokens </h2>
+			<UserData />
+			<Balance />
+		</div>
+	);
 }
 
 export default App;
