cdd4d320410a9fdb9b5f8be7005266a98d55eb98
Aegis <killcrafter.lol@gmail.com>
2021-10-19 23:49:49 +1100

    Migration structure React

--- src/index.js
+++ src/index.js
@@ -2,38 +2,11 @@ import "./config"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Log from './Log';
 import App from './App';
-import Market from './Market';
-
-import Discord from './community/Discord'
-import Contacts from "./community/Contacts";
-
-ReactDOM.render(
-<React.StrictMode>
-	<Log />
-</React.StrictMode>,
-document.getElementById('btn')
-);
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-document.getElementById('accountData')
-);
-
-ReactDOM.render(
-<React.StrictMode>
-	<Market />
-</React.StrictMode>,
-document.getElementById('marketData')
-);
-
-ReactDOM.render(
-	<React.StrictMode>
-		<Discord />
-		<Contacts />
-	</React.StrictMode>,
-	document.getElementById('communityAndFeedback')
+document.getElementById('root')
 );
