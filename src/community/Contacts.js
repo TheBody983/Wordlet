6594c754887c3452bf57e94f84450e883bc9a85c
Aegis <killcrafter.lol@gmail.com>
2021-10-19 23:49:08 +1100

    Ajout cadre autour des éléments de communauté

--- src/community/Contacts.js
+++ src/community/Contacts.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const Contacts = () => {
     return (
-        <div id = "contact">
-            <ul><a href="mailto:wordletthewordwallet@gmail.com" ><img src = './mail.png' class="mail" alt=""/></a></ul>
-            <ul><a href="https://discord.gg/zmRRJcVJzM"><img src = "<>discord.png" class="discord"  alt=""/></a></ul>
-            <ul><a href="https://www.linkedin.com/company/wordlet/"><img src = "./linkedIn.png" class="linkedIn" alt=""/></a></ul>
-            <ul><a href="https://www.facebook.com/WordletTheWordWallet"><img src = "./fb.png" class="fb" alt=""/></a></ul>
-        </div>
+        <ul id="contacts">
+            <li class="card"><img src="mail.png"/><a href="mailto:wordletthewordwallet@gmail.com" >mail</a></li>
+            <li class="card"><a href="https://discord.gg/zmRRJcVJzM">Discord</a></li>
+            <li class="card"><a href="https://www.linkedin.com/company/wordlet/">LinkedIn</a></li>
+            <li class="card"><a href="https://www.facebook.com/WordletTheWordWallet">Facebook</a></li>
+        </ul>
     )
 }
 
--- src/community/Discord.js
+++ src/community/Discord.js
@@ -3,7 +3,7 @@ import React from "react"
 const Discord = () => {
 return(
 <div className="discord">
-<iframe src="https://discord.com/widget?id=817218229377957938&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" title="discord">test</iframe>
+<iframe src="https://discord.com/widget?id=817218229377957938&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" title="discord" className="card"/>
 </div>
 )
 }
