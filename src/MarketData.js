aea1b0dd5efee0b3257ea31ac581626a315adc15
Lucas <80731304+Lucky314159@users.noreply.github.com>
2021-10-23 10:03:40 +1100

    Update MarketData.js

--- src/MarketData.js
+++ src/MarketData.js
@@ -1,62 +1,58 @@
 import React, { useState, useEffect } from "react";
 
-import checkTokensForSale from "./cadence/checkTokensForSale.script";
-import getTokenMetadata from "./cadence/getTokenMetadata.script";
-import getTokenPrice from "./cadence/getTokenPrice.script";
-import buyToken from "./cadence/buyToken.tx";
+import checkTokensForSale from "../../cadence/checkTokensForSale.script";
+import getTokenMetadata from "../../cadence/getTokenMetadata.script";
+import getTokenPrice from "../../cadence/getTokenPrice.script";
+import buyToken from "../../cadence/buyToken.tx";
 
-import * as fcl from "@onflow/fcl";
 
 const MarketData = () => {
 const [tokensToSell, setTokensToSell] = useState([])
+useEffect(() => checkMarketplace(), []);
 
-useEffect(() => {
-	checkMarketplace(user.addr);
-}, []);
-
-const [user, setUser] = useState({loggedIn: null})
-useEffect(() => fcl.currentUser().subscribe(setUser), [])
+let address = "0x1f7da62a915f01c7"
 
 const checkMarketplace = async () => {
-	try {
-		// Récupère les IDs des tokens à vendre sur le compte wordlet
-		const tokens = await checkTokensForSale(user.addr)
-
-		let marketplaceMetadata = [];
-		// Récupère les métadonnées de chaque token
-		for (const id of tokens) {
+	
+	// Récupère les IDs des tokens à vendre sur le compte wordlet
+	const tokens = await checkTokensForSale(address)
+	
+	let marketplaceMetadata = [];
+	// Récupère les métadonnées de chaque token
+	for (const id of tokens) {
 			const decodedMetadata = await getTokenMetadata(id)
 			decodedMetadata["price"] = await getTokenPrice(id)
 			decodedMetadata["id"] = id
 			marketplaceMetadata.push(decodedMetadata);
 		}
 		setTokensToSell(marketplaceMetadata);
-	} catch (error) {
-		console.error(error)
-	}
-};
-
+		
+	};
+	
 return (
-	<div className="market-listings">
-	{
-		tokensToSell.map(token => {
-		return (
-			<div key={token.id} className="listing">
-			
-				<h3>{token.mot}</h3>
-				<p>{token.id}</p>
-				<p>Source: {token.source}</p>
-				<p>Type: {token.type}</p>
-				
-				<h4>Price</h4>
-				<p>{parseInt(token.price, 10).toFixed(2)} WOT</p>
-				<button onClick={() => buyToken(token.id, user.addr)}>Acheter</button>
-				
-			</div>
-		)
-		})
-	}
-	</div>
+	<>
+		Tokens de {address}
+		<div className="market-listing">
+			{
+			tokensToSell.map(token => {
+				return (
+					<div key={token.id} className="card">
+					
+						<h3>{token.mot}</h3>
+						<p>#{token.id}</p>
+						<p>Source: {token.source}</p>
+						<p>Type: {token.type}</p>
+						
+						<h4>Price</h4>
+						<p>{parseInt(token.price, 10).toFixed(2)} WOT</p>
+						<button onClick={() => buyToken(token.id, address)}>Acheter</button>
+						
+					</div>
+				)
+			})
+			}
+		</div>
+	</>
 );
 };
 
