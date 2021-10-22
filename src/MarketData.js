b4c6c4d7194abb2022a3477deb4eb06d39d38c77
Lucas <80731304+Lucky314159@users.noreply.github.com>
2021-10-23 09:57:08 +1100

    Update MarketData.js

--- src/MarketData.js
+++ src/MarketData.js
@@ -7,62 +7,57 @@ import buyToken from "./cadence/buyToken.tx";
 
 import * as fcl from "@onflow/fcl";
 
-const MarketData = (address) => {
-	
-	const [tokensToSell, setTokensToSell] = useState([])
+const MarketData = () => {
+const [tokensToSell, setTokensToSell] = useState([])
 
-	address = "0x5be6fee0409b4842"
+useEffect(() => {
+	checkMarketplace(user.addr);
+}, []);
 
-	useEffect(() => {
-		checkMarketplace();
-	}, []);
+const [user, setUser] = useState({loggedIn: null})
+useEffect(() => fcl.currentUser().subscribe(setUser), [])
 
-	const [user, setUser] = useState({loggedIn: null})
-	useEffect(() => fcl.currentUser().subscribe(setUser), [])
+const checkMarketplace = async () => {
+	try {
+		// Récupère les IDs des tokens à vendre sur le compte wordlet
+		const tokens = await checkTokensForSale(user.addr)
 
-	
-	const checkMarketplace = async () => {
-
-			// Récupère les IDs des tokens à vendre sur le compte wordlet
-			const tokens = await checkTokensForSale(address)
-
-			let marketplaceMetadata = [];
-			// Récupère les métadonnées de chaque token
-			for (const id of tokens) {
-				const decodedMetadata = await getTokenMetadata(id)
-				decodedMetadata["price"] = await getTokenPrice(id)
-				decodedMetadata["id"] = id
-				marketplaceMetadata.push(decodedMetadata);
-			}
-			setTokensToSell(marketplaceMetadata);
-
-	};
-
-	return (
-
-		<div className="market-listings">
-			
-		<p> Tokens de {address} </p>
-		{
-			tokensToSell.map(token => {
-				return (
-					<div key={token.id} className="listing">
-					
-						<h3>{token.mot}</h3>
-						<p>{token.id}</p>
-						<p>Source: {token.source}</p>
-						<p>Type: {token.type}</p>
-						
-						<h4>Price</h4>
-						<p>{parseInt(token.price, 10).toFixed(2)} WOT</p>
-						<button onClick={() => buyToken(token.id, address)}>Acheter</button>
-						
-					</div>
-				)
-			})
+		let marketplaceMetadata = [];
+		// Récupère les métadonnées de chaque token
+		for (const id of tokens) {
+			const decodedMetadata = await getTokenMetadata(id)
+			decodedMetadata["price"] = await getTokenPrice(id)
+			decodedMetadata["id"] = id
+			marketplaceMetadata.push(decodedMetadata);
 		}
-		</div>
-	);
+		setTokensToSell(marketplaceMetadata);
+	} catch (error) {
+		console.error(error)
+	}
+};
+
+return (
+	<div className="market-listings">
+	{
+		tokensToSell.map(token => {
+		return (
+			<div key={token.id} className="listing">
+			
+				<h3>{token.mot}</h3>
+				<p>{token.id}</p>
+				<p>Source: {token.source}</p>
+				<p>Type: {token.type}</p>
+				
+				<h4>Price</h4>
+				<p>{parseInt(token.price, 10).toFixed(2)} WOT</p>
+				<button onClick={() => buyToken(token.id, user.addr)}>Acheter</button>
+				
+			</div>
+		)
+		})
+	}
+	</div>
+);
 };
 
 export default MarketData;
