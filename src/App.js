ccd914ea46c2ff32939286d092420b9149280238
Aegis <killcrafter.lol@gmail.com>
2021-10-23 20:19:57 +1100

    quick fix
    alt on images
    remove images from contact

--- src/App.js
+++ src/App.js
@@ -15,21 +15,21 @@ function App() {
 		</header>
 		<section id="accueil">
 			<Accueil/>
-            <img src="etoiles.png" id="stars"/>
-            <img src="lune.png" id="moon"/>
-            <img src="montagne_derriere.png" id="mountains_behind"/>
+            <img src="etoiles.png" id="stars" alt=""/>
+            <img src="lune.png" id="moon" alt=""/>
+            <img src="montagne_derriere.png" id="mountains_behind" alt=""/>
 		</section>
 		<section id="marche">
 			<Market />
-			<img src="marche.png" id="marche"/>
+			<img src="marche.png" id="marche" alt=""/>
 		</section>
 		<section id="collection">
 			<UserData />
-			<img src="cave.png" id="mountains_front"/>
+			<img src="cave.png" id="mountains_front" alt=""/>
 		</section>
 		<section id="communaute">
 			<Community />
-			<img src="lava1.jpg" id="mountains_front"/>
+			<img src="lava1.jpg" id="mountains_front" alt=""/>
 		</section>
 		<section id="apropos">
 			<APropos/>
