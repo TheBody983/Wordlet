273f15955dfeec040ee0a1b1339d9d6ceeadc483
Aegis <killcrafter.lol@gmail.com>
2021-10-29 06:25:43 +1100

    Simplified AuthCluster

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -1,56 +1,24 @@
 import React, {useState, useEffect} from 'react'
 import * as fcl from "@onflow/fcl"
 
-import userIsinitialized from '../../cadence/userIsInitialized.script'
-import createWOTVault from "../../cadence/createWOTVault.tx";
-import createWordletCollection from "../../cadence/createWordletCollection.tx";
 import { useAuth } from '../../providers/AuthProvider';
+import { useUser } from '../../providers/UserProvider';
 
 const Accueil = () => {
     const { user, loggedIn, logOut } = useAuth()
-
-	const [state, setState] = useState("Not Ready")
-	
-	userIsinitialized(user?.addr)
-		.then((result)=>{
-			if(result) setState("Ready")
-		})
-
-        if (loggedIn && state === "Ready") {
-            return (
-                <>
-                    <h1 id="wordlet-title">Wordlet</h1>
-                    <div id="auth-cluster">
-                        <span>{user?.addr ?? "Pas d'Adresse"} - {state}</span>
-                        <button className="btn-secondary" onClick={ () => logOut() }>Déconnexion</button>
-                    </div>
-                </>
-                )
-        } 
-        else if(loggedIn){
-            return (
-                <>
-                    <h1 id="wordlet-title">Wordlet</h1>
-                    <div id="auth-cluster">
-                        <span>{user?.addr ?? "Pas d'Adresse"} - {state}</span>
-                        <button className="btn-secondary" onClick={fcl.unauthenticate}>Déconnexion</button>
-                        <div id="setup-account-div">
-                            <p> Setup </p>
-                            <button onClick={() => createWOTVault()}>WOToken</button>
-                            <button onClick={() => createWordletCollection()}>Wordlet Wallet</button>
-                        </div>
-                    </div>
-                </>
-                )
-        }
-        else {
-            return (
-                <>
-                    <h1 id="wordlet-title">Wordlet</h1>
-                    <button className="btn-primary" id="auth-cluster" onClick={fcl.authenticate}>Connexion</button>
-                </>
-            )
-        }
+    return(
+        <>
+            <h1 id="wordlet-title">Wordlet</h1>
+            {loggedIn?
+                <div id="auth-cluster">
+                    <span>{user?.addr ?? "Pas d'Adresse"}</span>
+                    <button className="btn-secondary" onClick={ () => logOut() }>Déconnexion</button>
+                </div>
+            :
+                <button className="btn-primary" id="auth-cluster" onClick={fcl.authenticate}>Connexion</button>
+            }
+        </>
+    )
 }
 
 export default Accueil
