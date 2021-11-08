73f3963f89e1887b6322a2d56dfe4870b31b59c3
Aegis <killcrafter.lol@gmail.com>
2021-11-09 10:53:00 +1100

    Update index.js
    Indent fixes

--- src/index.js
+++ src/index.js
@@ -11,22 +11,22 @@ import Advanced from './components/Advanced';
 import Decoy from './components/Decoy';
 
 ReactDOM.render(
-	<BrowserRouter>
-    <Routes>
-      <Route path = "/" element={<App />}/>
-      <Route
-        path="*"
-        element = {
-          <section>
-            <h1>Wordlet 404 page not found</h1>
-          </section>
-        }
-      />
-	    <Route path = "about" element = {<About />} />
-	    <Route path = "forge" element = {<Forge />} />
-	    <Route path = "advanced" element = {<Advanced />} />
-      <Route path = "decoy" element = {<Decoy />} />
-    </Routes>
-  </BrowserRouter>,
+    <BrowserRouter>
+        <Routes>
+            <Route path = "/" element={<App />}/>
+            <Route
+                path="*"
+                element = {
+                <section>
+                    <h1>Wordlet 404 page not found</h1>
+                </section>
+                }
+            />
+                <Route path = "about" element = {<About />} />
+                <Route path = "forge" element = {<Forge />} />
+                <Route path = "advanced" element = {<Advanced />} />
+            <Route path = "decoy" element = {<Decoy />} />
+        </Routes>
+    </BrowserRouter>,
 document.getElementById('root')
 ); 
