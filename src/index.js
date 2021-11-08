b92f7148f0c9b4b128250418b6e3b0aa220b76e3
LudovicTronel <ludo.tronel@outlook.fr>
2021-11-08 15:12:04 +1100

    Mise en place routage basique
    - Installation des dépendances react-router-dom à jour
    - Mise en place de l’architecture de routage dans « index.js » + gestion d’erreur de page non trouvée
    - Création de components About, Forge et Decoy afin de recueillir les différentes fonctionnalités des éléments
    - Ajout des liens de routage vers la Forge et A propos dans le Header
    - Mise à jour du pitch dans l’onglet À propos
    - Suppression du component « A propos » dans App.js
    - Ajout du component et du routage de Advanced (utilisateur avancé)
    - Ajout du header dans les pages créées
    - Routage de l’élément « Accueil » afin d’y avoir accès à partir des pages externes
    - Création d’un espace de jeux dans l’Accueil
    - Création d’un espace Credits pour les mentions légales dans l’Accueil
    - Réparation (provisoire) du CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import "./config"
+import "./config";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import App from './App';
+import About from './components/About';
+import Forge from './components/Forge';
+import Advanced from './components/Advanced';
+import Decoy from './components/Decoy';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+    <Routes>
+      <Route path = "/" element={<App />}>
+        <Route
+          path="*"
+          element = {
+            <main style = {{ padding: "1rem" }}>
+              <p>Wordlet 404 page not found</p>
+            </main>
+          }
+        />
+      </Route>
+	  <Route path = "/about" element = {<About />} />
+	  <Route path = "/forge" element = {<Forge />} />
+	  <Route path = "/advanced" element = {<Advanced />} />
+    <Route path = "/decoy" element = {<Decoy />} />
+    </Routes>
+  </BrowserRouter>,
 document.getElementById('root')
-);
+); 
