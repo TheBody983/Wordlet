128ed0bcfef3bada9391097a6cf295937bba0872
Lucky314159 <80731304+Lucky314159@users.noreply.github.com>
2021-12-10 11:30:51 +1100

    patch erreurs pour compte non initialisés

--- src/cadence/get-user-salelist.script.js
+++ src/cadence/get-user-salelist.script.js
@@ -2,10 +2,15 @@ export const GET_USER_SALELIST=`
 import MarketplaceContract002 from 0x1f7da62a915f01c7
 
 pub fun main(address: Address) : [UInt64]? {
-    let owner = getAccount(address)
-    let ref = owner.getCapability<&{MarketplaceContract002.SalePublic}>(MarketplaceContract002.SaleCollectionPublicPath).borrow()
-        ?? panic("Could not borrow account nft sale collection reference")
+	let acct = getAccount(address)
+	let cap = acct.getCapability<&{MarketplaceContract002.SalePublic}>(MarketplaceContract002.SaleCollectionPublicPath)
 
-    return ref.getIDs()
+	if(cap.check()){
+		let ref = cap.borrow()
+			?? panic("Could not borrow account nft sale collection reference")
+
+		return ref.getIDs()
+	}
+	return nil
 }
 `
