f21a92707f1927d0f900d4591c3bb7467809fc86
Lucky314159 <80731304+Lucky314159@users.noreply.github.com>
2021-12-15 13:25:14 +1100

    maj repartition mots

--- ps_scripts/test.ps1
+++ ps_scripts/test.ps1
@@ -18,23 +18,21 @@ $conn = New-Object MySql.Data.MySqlClient.MySqlConnection($connectionString)
 $conn.Open()
 
 $query = "
-(select mot from `lexique_francais` where cgram regexp 'NOM' and freqlivres > 10 and mot not regexp ' ' order by rand() limit 6)
+(select mot from `lexique_francais` where cgram regexp 'NOM' and freqlivres > 10 and mot not regexp ' ' order by rand() limit 4)
 union
-(select mot from `lexique_francais` where cgram regexp 'VER' and freqlivres > 10 and mot not regexp ' ' order by rand() limit 3)
+(select mot from `lexique_francais` where cgram regexp 'VER' and freqlivres > 10 and mot not regexp ' ' order by rand() limit 4)
 union
-(select mot from `lexique_francais` where cgram regexp 'ART|PRO' and freqlivres > 200 and mot not regexp ' ' order by rand() limit 2)
+(select mot from `lexique_francais` where cgram regexp 'ART|PRO' and freqlivres > 200 and mot not regexp ' ' order by rand() limit 4)
 union
-(select mot from `lexique_francais` where cgram regexp 'CON|PRE' and freqlivres > 10 and mot not regexp ' ' order by rand() limit 2)
+(select mot from `lexique_francais` where cgram regexp 'CON|PRE' and freqlivres > 400 and mot not regexp ' ' order by rand() limit 4)
 union
-(select mot from `lexique_francais` where cgram regexp 'ADV|ADJ' and freqlivres > 20 and mot not regexp ' ' order by rand() limit 2);
+(select mot from `lexique_francais` where cgram regexp 'ADV|ADJ' and freqlivres > 20 and mot not regexp ' ' order by rand() limit 4);
 "
 
 $MysqlCmd = New-Object MySql.Data.MySqlClient.MySqlCommand($query, $conn)    
 $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($MysqlCmd) 
 $DataSet = New-Object System.Data.DataSet  
 $DataAdapter.Fill($DataSet, "data")
-
-flow transactions send --network testnet --signer wordlet ./cadence/transactions/MarketPlace/empty-marketplace.tx.cdc
 
 Foreach($word in $DataSet.Tables[0].Rows.mot) 
 { 
